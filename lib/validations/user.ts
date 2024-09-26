0d4fe73e9d49664aafd13ad8115ba6378a7d0364
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-09-27 00:38:16 +0500

    chore: remove unnecessary files

--- lib/validations/user.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-
-export const UserValidation = z.object({
-  name: z.string().min(3).max(100),
-  username: z.string().min(3).max(100),
-  bio: z.string().min(3).max(1000),
-  image: z.string(),
-});
