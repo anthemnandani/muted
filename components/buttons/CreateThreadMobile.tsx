8d89c9fc3ed0716f46fb77d9f0eab34e0a5379b6
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2025-01-18 07:13:39 +0500

    chore: use plus icon in mobile thread button

--- components/buttons/CreateThreadMobile.tsx
+++ components/buttons/CreateThreadMobile.tsx
@@ -4,7 +4,7 @@ import { Icons } from '../icons';
 const CreateThreadMobile = () => {
   return (
     <div className='relative md:hidden w-15 h-12 flex-center rounded-xl hover:bg-primary transition-colors duration-150'>
-      <Icons.create className='size-6 text-secondary' />
+      <Icons.plus className='size-6 text-secondary' />
     </div>
   );
 };
