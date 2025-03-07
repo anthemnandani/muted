bea5685608fe6205423a96dfa2734ef5b96d7929
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2025-03-07 20:27:23 +0500

    chore(user-profile): delete unused files

--- lib/types.ts
+++ lib/types.ts
@@ -518,7 +518,5 @@ export interface PostNavigatorContextType {
   isFirstPost: boolean;
   isLastPost: boolean;
   currentIndex: number;
-  setIsFirstPost: (isFirstPost: boolean) => void;
-  setIsLastPost: (isLastPost: boolean) => void;
   setPostNavigation: (index: number, total: number) => void;
 }
